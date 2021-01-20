b26a33c68a2329f144becc074230585161cc0d86
exexxv25 <exexxv@gmail.com>
2021-01-20 11:46:07 -0300

    fix script deploy qa

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,11 +6,12 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git fetch origin deploy
-    git reset --hard origin/deploy
+    # git fetch origin deploy
+    # git reset --hard origin/deploy
+    git pull
 
     # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+    composer update
 
     # Migrate database
     php artisan migrate --force
