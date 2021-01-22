2423c9d7877ba19739942bc0f77c189b88e0c8c1
root <root@neighbors.southamerica-east1-b.c.double-kite-301817.internal>
2021-01-22 06:08:38 +0000

    Fix entorno

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,25 +4,28 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down ) || true
     # Update codebase
     # git fetch origin deploy
     # git reset --hard origin/deploy
-    git pull
+    git fetch --all
 
     # Install dependencies based on lock file
-    composer update
+    composer update -n
 
     # Migrate database
     php artisan migrate --force
 
     # Note: If you're using queue workers, this is the place to restart them.
     # ...
-
+    php artisan l5-swagger:generate
+    
     # Clear cache
     php artisan optimize
 
 # Exit maintenance mode
 php artisan up
+
+php artisan l5-swagger:generate
 
 echo "Application deployed!"
